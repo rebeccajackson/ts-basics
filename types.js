48859524301ee76a9fcd9aedd5e06e7490b792d5
Rebecca Jackson <zimbecs@gmail.com>
2019-03-28 15:17:20 +1300

    type assertion

---
+++ types.js
@@ -0,0 +1,22 @@
+var myNum;
+var myBool;
+var myVar;
+var myString;
+// let strArr: string[]
+// let numArr: number[]
+// let boolArr: boolean[]
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
+myString = 'Hello'.slice(0, 3);
+myNum = 22;
+myBool = true;
+myVar = {};
+strArr = ['hello', 'world'];
+numArr = [2, 6, 5, 8];
+boolArr = [true, false, false, true];
+strNumTuple = ['Hello', 3];
+var myVoid = undefined;
+var myNull = undefined;
+console.log(myNull);
---
+++ types.ts
@@ -0,0 +1,29 @@
+let myNum: number
+let myBool: boolean
+let myVar: any
+let myString: string
+
+// let strArr: string[]
+// let numArr: number[]
+// let boolArr: boolean[]
+
+let strArr: Array<string>
+let numArr: Array<number>
+let boolArr: Array<boolean>
+let strNumTuple: [string, number]
+
+myString = 'Hello'.slice(0, 3)
+myNum = 22
+myBool = true
+myVar = {}
+
+strArr = ['hello', 'world']
+numArr = [2,6,5,8]
+boolArr = [true, false, false, true]
+
+strNumTuple = ['Hello', 3]
+
+let myVoid: void = undefined
+let myNull: null = undefined
+
+console.log(myNull)
